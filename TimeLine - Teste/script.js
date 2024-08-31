7eff505d528f6db89573fbf6013ce957fcef04c9
Byorks <vanessabyorksp@gmail.com>
2024-08-31 17:32:44 -0300

    :sparkles: feat: Audio: Barra de Progresso

--- TimeLine - Teste/script.js
+++ TimeLine - Teste/script.js
@@ -18,7 +18,7 @@ document.addEventListener('scroll', function() {
 
 // Player de áudio
 
-let mysong = document.getElementById("mysong");
+let mysong = document.getElementById('mysong');
 let icon = document.getElementById("icon");
 
 // Play/pause
@@ -38,3 +38,22 @@ let recent_volume = document.querySelector('#volume');
 const volume_change = () => {
     mysong.volume = recent_volume.value / 100;
 }
+
+// Seletor de progresso da música
+let slider = document.querySelector('#duration_slider');
+const change_duration = () => {
+    slider_position = mysong.duration * (slider.value/100);
+    mysong.currentTime = slider_position;
+}
+
+// Atualizar a posição do slider de acordo com a música
+const updateSlider = () => {
+    let position = 0;
+
+    if(!isNaN(mysong.duration)){
+        position = mysong.currentTime * (100/ mysong.duration);
+        slider.value = position;
+    }
+}
+
+mysong.addEventListener('timeupdate', updateSlider);
